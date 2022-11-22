f74a5c57f5c8631c5124311835376a854daeb061
Niño Arenas <ntb.arenas@protonmail.com>
2022-11-22 20:52:58 +0000

    update weather

--- main.js
+++ main.js
@@ -7,41 +7,48 @@ async function loadWeather(city) {
   document.querySelector("#container-weather").classList.remove("d-block");
   document.querySelector("#container-weather").classList.add("d-none");
 
-  setTimeout(displayWeather, 2000);
   let res = await fetch(`https://weatherdbi.herokuapp.com/data/weather/${city}`);
   weather = await res.json();
+
+  setTimeout(displayWeather, 500);
 
   console.log(weather);
 }
 
 const searchValue = document.querySelector("#search-bar");
 searchValue.addEventListener("change", (e) => {
-  loadWeather(searchValue.value.toLowerCase());
+  if (searchValue.value === "") {
+    loadWeather("lisbon");
+  } else {
+    loadWeather(searchValue.value.toLowerCase());
+  }
 });
-
-// const searchBar = () => {
-//   loadWeather(searchValue.value.toLowerCase());
-// };
 
 const displaySky = () => {
   const skyCoverIconEL = document.querySelector("#sky-cover-icon");
-  const weatherSky = weather.currentConditions.comment.toLowerCase();
 
-  if (weatherSky === "clear" || weatherSky === "sunny") {
-    skyCoverIconEL.innerHTML = `<i class="bi bi-sun"></i>`;
-  } else if (weatherSky === "cloudy") {
-    skyCoverIconEL.innerHTML = `<i class="bi bi-clouds"></i>`;
-  } else if (weatherSky === "partly cloudy" || weatherSky === "clear with periodic clouds" || weatherSky === "mostly cloudy") {
-    skyCoverIconEL.innerHTML = `<i class="bi bi-cloud-sun"></i>`;
-  } else if (weatherSky === "light rain showers") {
-    skyCoverIconEL.innerHTML = `<i class="bi bi-cloud-drizzle"></i>`;
-  } else if (weatherSky === "snow showers") {
-    skyCoverIconEL.innerHTML = `<i class="bi bi-cloud-snow"></i>`;
-  } else if (weatherSky === "rain") {
-    skyCoverIconEL.innerHTML = `<i class="bi bi-cloud-rain"></i>`;
-  } else {
-    skyCoverIconEL.innerHTML = `<i class="bi bi-cloud-lightning-rain"></i>`;
-  }
+  skyCoverIconEL.innerHTML = `<img src="${weather.currentConditions.iconURL}" class="img-fluid" alt="">`;
+};
+
+const displayDailyForecast = () => {
+  const htmlString = weather.next_days
+    .map((weather) => {
+      return `
+        <div class="col-1 border rounded py-3"> 
+          <p class="text-center">${weather.day}</p>
+          <div class="text-center">
+            <img src="${weather.iconURL}" class="img-fluid" alt="" />
+          </div>
+          <div class="text-center">
+            <span>${weather.max_temp.c}º</span>
+            <span>${weather.min_temp.c}º</span>
+          </div>
+        </div>
+        `;
+    })
+    .join("");
+
+  document.querySelector("#daily-forecast").innerHTML = htmlString;
 };
 
 const displayWeather = () => {
@@ -71,6 +78,7 @@ const displayWeather = () => {
   citySkyCoverEL.innerHTML = `${weather.currentConditions.comment}`;
 
   displaySky();
+  displayDailyForecast();
 };
 
 loadWeather("lisbon");
